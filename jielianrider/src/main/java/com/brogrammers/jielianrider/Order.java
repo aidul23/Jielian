036fd461dc0da5f2804aefd1ace547f9b94a284f
ASUS <aidul5613@gmail.com>
2021-06-02 13:20:22 +0600

    rider

---
+++ jielianrider/src/main/java/com/brogrammers/jielianrider/Order.java
@@ -0,0 +1,40 @@
+package com.brogrammers.jielianrider;
+
+public class Order {
+
+    private String orderId, orderDateTime, pickupAddress, destinationAddress, orderStatus;
+            int orderTotalCost;
+
+    public Order(String orderId, String orderDateTime, String pickupAddress, String destinationAddress, String orderStatus, int orderTotalCost) {
+        this.orderId = orderId;
+        this.orderDateTime = orderDateTime;
+        this.pickupAddress = pickupAddress;
+        this.destinationAddress = destinationAddress;
+        this.orderStatus = orderStatus;
+        this.orderTotalCost = orderTotalCost;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public String getOrderDateTime() {
+        return orderDateTime;
+    }
+
+    public int getOrderTotalCost() {
+        return orderTotalCost;
+    }
+
+    public String getPickupAddress() {
+        return pickupAddress;
+    }
+
+    public String getDestinationAddress() {
+        return destinationAddress;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+}
